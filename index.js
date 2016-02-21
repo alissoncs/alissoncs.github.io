bb23d407707334936a09f172238eff6264dadd73
Alisson Carvalho <webalissoncs@gmail.com>
2016-02-20 23:05:03 -0200

    sample

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express');
-
+var swig = require('swig');
 var app = express();
 
+app.use('/static', express.static(__dirname + 'dest'));
+
 app.get('/', function(request, response){
-	response.send("olá mundo em node");
+	response.send("olá mundo em node 2");
 });
+
+app.listen(8000);
+
+console.log('Server running at http://127.0.0.1:8000');
