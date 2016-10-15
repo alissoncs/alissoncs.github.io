c852d38aec2d3a9596515cad011784fbea616c01
Alisson Carvalho <webalissoncs@gmail.com>
2016-10-15 12:17:40 -0300

    root page ready to work

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require( 'express' )
 
 const app = express()
+const port = 9010;
 
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/site/views')
 app.use('/css', express.static(__dirname + '/site/css'))
 app.use('/js', express.static(__dirname + '/site/js'))
 app.use('/img', express.static(__dirname + '/site/img'))
+app.use('/', express.static(__dirname + '/site/root'))
 
 app.get('*', (req,res,next) => {
 
@@ -14,6 +16,6 @@ app.get('*', (req,res,next) => {
 
 })
 
-app.listen(9010, () => {
-    console.log('Listening at 80')
+app.listen(port, () => {
+    console.log('Listening at ' + port)
 })
