83429344358f627afce6e36741b359de1c9ec109
Alisson da Silva <alisson.silva@4all.com>
2017-10-03 18:24:18 -0300

    fix project

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,54 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const pug = require('gulp-pug')
+const connect = require('gulp-connect')
+const babel = require('gulp-babel')
 
-gulp.task('sass', function() {
 
-    return gulp.src('./site/_sass/main.scss')
+gulp.task('connect', () => {
+    connect.server({
+        root: './dest',
+        livereload: true,
+        port: 3083,
+    });
+})
+
+gulp.task('pug', () => {
+    return gulp.src('./site/views/*.pug')
+    .pipe(pug({
+        // pug options here ! :D
+    }))
+    .pipe(gulp.dest('./dest/'))
+})
+
+gulp.task('js', () => {
+    return gulp.src('./site/js/main.js')
+    .pipe(babel({
+        presets: ['env'],
+    }))
+    .pipe(gulp.dest('./dest/js'))
+})
+
+gulp.task('sass', () => {
+
+    return gulp.src('./site/styles/main.scss')
     .pipe(sass({
         includePaths: [
           './node_modules/normalize-sass',
           './node_modules/bootstrap-sass/assets/stylesheets',
         ]
     }).on('error', sass.logError))
-    .pipe(gulp.dest('./site/css'))
+    .pipe(gulp.dest('./dest/css'))
 
 })
-gulp.task('default', ['sass'])
+gulp.task('img', () => {
+    gulp.src('./site/img/**.*')
+    .pipe(gulp.dest('./dest/img'))
+})
+gulp.task('default', [ 'pug', 'sass', 'js', 'img' ])
 
-gulp.task('watch', ['default'], function() {
-    gulp.watch('./site/_sass/**/*.scss', ['sass'])
+gulp.task('watch', [ 'default', 'connect'], function() {
+    gulp.watch('./site/styles/**/*.scss', ['sass'])
+    gulp.watch('./site/views/**/*.pug', ['pug'])
+    gulp.watch('./site/js/**/*.js', ['js'])
 })
